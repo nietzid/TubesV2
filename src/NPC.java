061af4ab5acb41a45f691912a0e92687eddffdb3
shintazahra <zshinta06@gmail.com>
2021-05-06 22:38:59 +0700

    liat lagi prosesAksi NPC nyaa

--- src/NPC.java
+++ src/NPC.java
@@ -1,2 +1,68 @@
+import java.util.ArrayList;
+
 public class NPC extends ElemenGame{
+    private ArrayList<Item> loot = new ArrayList<>();
+    private Wilayah wilAktif;
+    private boolean isKnow = false;
+
+    public NPC(String nama, String deskripsi, Wilayah wilAktif){
+        super(nama, deskripsi);
+        this.wilAktif = wilAktif;
+    }
+
+    public void interaksi(Karakter karakter){
+        System.out.println("halo " +karakter.nama);
+        System.out.println("Selamat datang di wilayah " +wilAktif.nama);
+        System.out.println("wilayah ini adalah " +wilAktif.deskripsi);
+        System.out.println("anda ditugaskan untuk membunuh semua monster yang ada disini");
+        System.out.println("agar bisa menyelamatkan seluruh dunia ini");
+        System.out.println("untuk bisa membantu mengalahkan musuh anda harus memiliki item yang kuat");
+    }
+
+    @Override
+    public void initArrAksi() {
+        super.initArrAksi();
+        arrAksi.add(new Aksi("kenalan dengan NPC", 103, this));
+        arrAksi.add(new Aksi("lihat item yang dimiliki NPC",203,this));
+        arrAksi.add(new Aksi("minta item dari npc", 303, this));
+    }
+
+    public void prosesAksi(int idAksi) {
+        if (idAksi == 103) {
+            interaksi(Karakter);
+            isKnow = true;
+        }else if (idAksi == 203) {
+            if(isKnow){
+                printItem();
+            }else {
+                System.out.println("kenalan dulu yuk dangan NPC");
+            }
+        }else if (idAksi == 303){
+            if(isKnow){
+                giveItem(Item, Karakter);
+            }else {
+                System.out.println("kenalan dulu yuk dangan NPC");
+            }
+        }
+    }
+
+    public void printItem(){
+        for (Item item: loot){
+            System.out.printf("%d. %s \n",cc,item.getNama());
+            cc++;
+        }
+    }
+
+    public void giveItem(Item item, Karakter karakter){
+        karakter.addItem(item);
+        loot.remove(item);
+        if (loot.isEmpty()){
+            System.out.println("Item sudah diberikan semua");
+        }
+    }
+
+    public void addItem(Item item){
+        loot.add(item);
+    }
+
 }
--- src/Wilayah.java
+++ src/Wilayah.java
@@ -1,2 +1,85 @@
+import java.util.ArrayList;
+
 public class Wilayah extends ElemenGame{
+    private String misi;
+    private ArrayList<Item> loot = new ArrayList<>();
+    private ArrayList<Karakter> monsterDisini = new ArrayList<>();
+    private ArrayList<Karakter> arrNPC = new ArrayList<>();
+    private NPC warga;
+
+    public Wilayah (String nama,String deskripsi, String misi, NPC warga){
+        super(nama,deskripsi);
+        this.misi = misi;
+        this.warga = warga;
+    }
+
+    public void addItem(Item item){
+        loot.add(item);
+    }
+
+    public void removeItem(Item item){
+        loot.remove(item);
+    }
+
+    public void initArrAksi (){
+        super.initArrAksi();
+        arrAksi.add(new Aksi("interaksi dengan NPC",202,this));
+        arrAksi.add(new Aksi("Lihat musuh",302,this));
+        arrAksi.add(new Aksi("Serang musuh",402,this));
+        arrAksi.add(new Aksi("Lihat item tersedia",502,this));
+    }
+
+    public void prosesAksi(int idAksi){
+        //makasi arfi krn km baik meskipun sekali ak kasi gelar yg sm kaya ajin alias G.tng
+    }
+
+    public void addMonster(Karakter monster){
+        monsterDisini.add(monster);
+    }
+
+    public void removeMonster(Karakter monster){
+        monsterDisini.add(monster);
+    }
+
+    //Ssetter getter semuanya pokonya semuanya banget
+    public String getMisi() {
+        return misi;
+    }
+
+    public void setMisi(String misi) {
+        this.misi = misi;
+    }
+
+    public ArrayList<Item> getLoot() {
+        return loot;
+    }
+
+    public void setLoot(ArrayList<Item> loot) {
+        this.loot = loot;
+    }
+
+    public ArrayList<Karakter> getMonsterDisini() {
+        return monsterDisini;
+    }
+
+    public void setMonsterDisini(ArrayList<Karakter> monsterDisini) {
+        this.monsterDisini = monsterDisini;
+    }
+
+    public ArrayList<Karakter> getArrNPC() {
+        return arrNPC;
+    }
+
+    public void setArrNPC(ArrayList<Karakter> arrNPC) {
+        this.arrNPC = arrNPC;
+    }
+
+    public NPC getWarga() {
+        return warga;
+    }
+
+    public void setWarga(NPC warga) {
+        this.warga = warga;
+    }
+
 }
